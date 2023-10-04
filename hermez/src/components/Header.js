a6934abdbf923ba5e068ddc006d76ffc1f8f7c49
BrunoSerpa <serpabruno2005@gmail.com>
2023-10-04 11:05:16 -0300

    T12 - Adicionei o link para o perfil e para o home

--- hermez/src/components/Header.js
+++ hermez/src/components/Header.js
@@ -1,16 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './css/Header.css';
 
-import './css/Header.css'
-
+import sair from '../img/sair.png';
+import perfil from '../img/perfilHeader.png';
+import home from '../img/home.png';
 export default function Header(){
     return (
         <>
-            <header>
-                <h1>HEADER</h1>
+            <header className='hearderPadrão'>
                 <div>
-                    <Link to="/home">Home</Link>
-                    <Link to="/cadastro">Cadastro</Link>
-                    <Link to="/login">Login</Link>
+                    <Link to="/home">
+                        <img src={home}/>
+                        Início
+                    </Link>
+                    <Link to="/perfil">
+                        <img src={perfil}/>
+                        Perfil
+                    </Link>
+                    <Link to="/">
+                        <img src={sair}/>
+                        Sair
+                    </Link>
                 </div>
             </header>
         </>
