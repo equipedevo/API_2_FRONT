52d515a056ad2d08123922b1bc75ed5d63f0a79a
BrunoSerpa <serpabruno2005@gmail.com>
2023-10-04 10:50:09 -0300

    T05 - Adicionei as tags do formulário

--- hermez/src/pages/CadastroEmpresa.js
+++ hermez/src/pages/CadastroEmpresa.js
@@ -1,7 +1,6 @@
 import './css/CadastroEmpresa.css';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
-import Header from '../components/Header';
 import sair from '../img/sair.png';
 export default function CadastroEmpresa(){
     return (
@@ -18,19 +17,39 @@ export default function CadastroEmpresa(){
                 <form className='formEmpresaCadastro'>
                     <h1> Cadastre sua Empresa</h1>
                     <span for="nomeCompleto">NOME COMPLETO</span>
-                    <input type="text" placeholder='Nome' id="nomeCompleto" required/>
+                    <input
+                        type="text"
+                        placeholder='Nome'
+                        id="nomeCompleto"
+                        name='nomeCompleto'
+                    required/>
                     <div className='div2ColunasFormCadastro'>
                         <div>
                             <span for="senha">SENHA</span>
-                            <input type="password" placeholder='Senha' id="senha" required/>
+                            <input
+                                type="password"
+                                placeholder='Senha'
+                                id="senha"
+                                name='senha'
+                            required/>
                         </div>
                         <div>
                             <span for="senha">CONFIRMAR SENHA</span>
-                            <input type="password" placeholder='Senha' id="senhaConfirmada" required/>
+                            <input
+                                type="password"
+                                placeholder='Senha'
+                                id="senhaConfirmada"
+                                name='senhaConfirmada'
+                            required/>
                         </div>
                     </div>
-                    <span for="nomeCompleto">E-MAIL</span>
-                    <input type="email" placeholder='E-mail' id="email" required/>
+                    <span for="email">E-MAIL</span>
+                    <input
+                        type="email"
+                        placeholder='E-mail'
+                        id="email"
+                        name='email'
+                    required/>
                     <div className='divEnviarCadastroEmpresa'>
                         <input type='button' value='Enviar'/>
                     </div>
