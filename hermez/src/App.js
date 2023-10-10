ef5f4d061d8b4a06e261f23ddf3bcae9ee08442c
Mariana <mariana.izumiks@gmail.com>
2023-10-10 12:00:12 -0300

    T14 - Adicionando rota

--- hermez/src/App.js
+++ hermez/src/App.js
@@ -8,6 +8,7 @@ import LoginEmpresa from './pages/LoginEmpresa';
 import LoginUsuario from './pages/LoginUsuario';
 import CadastroEmpresa from './pages/CadastroEmpresa';
 import AbrirChamado from './pages/AbrirChamado';
+import AtenderChamado from './pages/AtenderChamados';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="chamado" element={< AbrirChamado />} />
           <Route path="cadastro" element={<LoginEmpresa />} />
           <Route path="cadastro" element={<Cadastro />} />
+          <Route path="dashboard-chamado" element={<AtenderChamado />} />
           {/* path="*" serve para qualquer rota, então deve ficar por último e direcionar para a home ou uma página de erro 404 */}
           <Route path="*" element={<LoginUsuario />} />
         </Route>
