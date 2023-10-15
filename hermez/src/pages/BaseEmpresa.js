b721a8d67d6382851160cc6e8aa7aa010baff889
BrunoSerpa <serpabruno2005@gmail.com>
2023-10-14 22:39:13 -0300

    T12 - Adicionei a base da empresa

---
+++ hermez/src/pages/BaseEmpresa.js
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom'
+
+import Header from '../components/HeaderEmpresa'
+import Footer from '../components/Footer'
+import './css/Base.css';
+
+export default function Home(){
+    if (localStorage.getItem('emp_cod') === null && localStorage.getItem('fun_cod') !== null) {
+        window.location.href = '/'
+    }
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+}
