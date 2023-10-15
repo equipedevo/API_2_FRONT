b55d4a4d0f7c706dd51fdfe8caabf199666aa6c7
BrunoSerpa <serpabruno2005@gmail.com>
2023-10-15 03:17:18 -0300

    T12 - Inseri critérios para dropdown de acordo com o cargo do funcionário;

--- hermez/src/components/Header.js
+++ hermez/src/components/Header.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Header.css';
 
@@ -10,94 +11,117 @@ import gerenciarUsuario from '../img/menu/gerenciarUsuario.png';
 import foneAtendimento from '../img/menu/foneAtendimento.png';
 import papelComLapis from '../img/menu/papelComLapis.png';
 
+function DropdownAdms() {
+    const [chamadosVisible, setChamadosVisible] = useState(false);
+    const [gerenciarUsuariosVisible, setGerenciarUsuariosVisible] = useState(false);
+
+    function toggleChamados() {
+        setChamadosVisible(!chamadosVisible);
+    }
+
+    function toggleGerenciarUsuarios() {
+        setGerenciarUsuariosVisible(!gerenciarUsuariosVisible);
+    }
+
+    
+    return (
+        <>
+            <button onClick={toggleChamados}>
+                <img src={balaoChat} alt="Chamados"/>
+                Chamados
+                <img
+                    src={dropdown}
+                    style={{
+                        transform: chamadosVisible ? 'rotate(180deg)' : 'rotate(0)',
+                    }}
+                    alt='dropdown'
+                />
+            </button>
+            <div
+                className="dropdownHeader"
+                id="Chamados"
+                style={{ display: chamadosVisible ? 'flex' : 'none' }}
+            >
+                <Link>Relatório</Link>
+                <Link>Meus Chamados</Link>
+            </div>
+            <button onClick={toggleGerenciarUsuarios}>
+                <img src={gerenciarUsuario} alt="Gerenciar Usuários" />
+                Gerenciar Usuários
+                <img
+                    src={dropdown}
+                    style={{
+                        transform: gerenciarUsuariosVisible ? 'rotate(180deg)' : 'rotate(0)',
+                    }}
+                    alt='dropdown'
+                />
+            </button>
+            <div
+                className="dropdownHeader"
+                id="GerenciarUsuarios"
+                style={{ display: gerenciarUsuariosVisible ? 'flex' : 'none' }}
+            >
+                <Link to="cadastro">Cadastrar</Link>
+                <Link>Alterar Dados</Link>
+            </div>
+        </>
+    );
+}
 export default function Header() {
-  function logout() {
-    localStorage.clear();
-    window.location.href = '/';
-  }
-  /* MOSTRAR dropdown */
-  function fechaDropdown(a) {
-    const elementA = document.getElementById(a);
-    const dropdownElement = document.getElementById(`dropdown${a}`);
-    if (a === 'Funcoes') {
-      document.getElementById('Chamados').style.display = "none";
-      document.getElementById('GerenciarUsuarios').style.display = "none";
-      document.getElementById('dropdownGerenciarUsuarios').style.transform = 'rotate(0)';
-      document.getElementById('dropdownChamados').style.transform = 'rotate(0)';
+    const cargo = localStorage.getItem('car_cod');
+    const [funcoesVisible, setFuncoesVisible] = useState(false);
+    function toggleGerenciarFuncoes() {
+        setFuncoesVisible(!funcoesVisible);
     }
-    if (elementA && elementA.style.display === "flex") {
-      elementA.style.display = "none";
-      if (dropdownElement) {
-        dropdownElement.style.transform = 'rotate(0)';
-      }
-    } else {
-      if (elementA) {
-        elementA.style.display = "flex";
-      }
-      if (dropdownElement) {
-        dropdownElement.style.transform = 'rotate(180deg)';
-      }
+    function logout() {
+        localStorage.clear();
+        window.location.href = '/';
     }
-  };
-  return (
+
+    return (
     <>
-      <header className='hearderPadrão'>
-        <div>
-          <Link to="./" className='linkHearderPadrão'>
+        <header className="hearderPadrão">
+        <Link to="./" className="linkHearderPadrão">
             <img src={home} alt="Início" />
             Início
-          </Link>
-          <div className="dropdownHeader alignCenter">
-            <button onClick={() => fechaDropdown('Funcoes')}>Suas Funções</button>
-            <div id='Funcoes'>
-              <button onClick={() => fechaDropdown('Chamados')}>
-                <img src={balaoChat} alt='Chamados' />
-                Chamados
-                <img src={dropdown} id='dropdownChamados'/>
-              </button>
-              <div className="dropdownHeader" id='Chamados'>
-                <Link>
-                    Relatório
+        </Link>
+        <div className="dropdownHeader alignCenter">
+            <button onClick={(toggleGerenciarFuncoes)}>Suas Funções</button>
+            <div
+                id='Funcoes'
+                style={{ display: funcoesVisible ? 'flex' : 'none' }}
+            >
+                {cargo === '3' ? (
+                    <DropdownAdms/>
+                ):(
+                    <Link>
+                        <img src={balaoChat} alt="BalaoChat" />
+                        <h1>Histórico de chamados</h1>
+                    </Link>
+                )}
+                {cargo !== '1' && (
+                <>
+                    <Link to="dashboard-chamado">
+                        <img src={foneAtendimento} alt="Atender Chamados" />
+                        Atender Chamados
+                    </Link>
+                </>
+                )}
+                <Link to="chamado">
+                    <img src={papelComLapis} alt="Novo Chamado" />
+                    Novo Chamado
                 </Link>
-                <Link>
-                    Meus Chamados
-                </Link>
-              </div>
-              <button onClick={() => fechaDropdown('GerenciarUsuarios')}>
-                <img src={gerenciarUsuario} alt='Gerenciar Usuários' />
-                Gerenciar Usuários
-                <img src={dropdown} id='dropdownGerenciarUsuarios' />
-              </button>
-              <div className="dropdownHeader" id='GerenciarUsuarios'>
-                <Link to='cadastro'>
-                    Cadastrar
-                </Link>
-                <Link>
-                    Alterar Dados
-                </Link>
-              </div>
-              <Link to="dashboard-chamado">
-                <img src={foneAtendimento} alt='Atender Chamados' />
-                Atender Chamados
-              </Link>
-              <Link to="chamado">
-                <img src={papelComLapis} alt='Novo Chamado' />
-                Novo Chamado
-              </Link>
             </div>
-          </div>
-          <Link className='linkHearderPadrão'>
-            <img src={perfil} alt='Perfil' />
-            Perfil
-          </Link>
-          <Link
-            className='linkHearderPadrão'
-            onClick={() => logout()}>
-            <img src={sair} alt='Sair' />
-            Sair
-          </Link>
         </div>
-      </header>
+        <Link className="linkHearderPadrão">
+            <img src={perfil} alt="Perfil" />
+            Perfil
+        </Link>
+        <Link className="linkHearderPadrão" onClick={logout}>
+            <img src={sair} alt="Sair" />
+            Sair
+        </Link>
+        </header>
     </>
   );
 }
