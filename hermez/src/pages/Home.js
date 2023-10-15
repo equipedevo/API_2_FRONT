1b4ee443f6d31e8122d35f41bab09012df42281a
BrunoSerpa <serpabruno2005@gmail.com>
2023-10-14 21:06:13 -0300

    T12 - Coloquei função de alterar botão; Troquei nome das classes; Coloqueis link das telas que gerenciam usuários e que visualizam chamados

--- hermez/src/pages/Home.js
+++ hermez/src/pages/Home.js
@@ -1,4 +1,5 @@
 import './css/Home.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import balaoChat from '../img/home/balaoChat.png';
@@ -7,6 +8,8 @@ import foneAtendimento from '../img/home/foneAtendimento.png';
 import papelComLapis from '../img/home/papelComLapis.png';
 
 export default function Home(){
+    const [chamados, setChamados] = useState(true);
+    const [gerenciar, setGerenciar] = useState(true);
     return (
         <>
             <div className='cabecalhoHeader'>
@@ -16,26 +19,62 @@ export default function Home(){
                     <h2>Acesse suas funções de uma maneira rápida e fácil!</h2>
                 </p>
             </div>
-            <div className='botoesHomePrincipal'>
-                <Link>
-                    <div>
-                        <img src={balaoChat}/>
-                        <h1> Visualizar chamados</h1>
+            <div className='divbotoesHome'>
+                {chamados ? (
+                    <Link
+                        className='botoesHome grande'
+                        onClick={()=> setChamados(!chamados)}
+                        >
+                        <div>
+                            <img src={balaoChat}/>
+                            <h1> Visualizar chamados</h1>
+                        </div>
+                    </Link>
+                ):(
+                    <div className='botoesPequenoHome' id='GerenciarUsuariosItens'>
+                        <Link className='botoesHome pequeno'>
+                            <div>
+                                <h1>Gerar relatórios</h1>
+                            </div>
+                        </Link>
+                        <Link className='botoesHome pequeno'>
+                            <div>
+                                <h1>Histórico meus chamados</h1>
+                            </div>
+                        </Link>
                     </div>
-                </Link>
-                <Link>
-                    <div>
-                        <img src={gerenciarUsuario}/>
-                        <h1> Gerenciar Usuário</h1>
+                )}
+                {gerenciar ? (
+                    <Link
+                        className='botoesHome grande'
+                        onClick={()=> setGerenciar(!gerenciar)}
+                    >
+                        <div>
+                            <img src={gerenciarUsuario}/>
+                            <h1>Gerenciar usuários</h1>
+                        </div>
+                    </Link>
+                ):(
+                    <div className='botoesPequenoHome' id='GerenciarUsuariosItens'>
+                        <Link className='botoesHome pequeno'>
+                            <div>
+                                <h1>Cadastrar usuários</h1>
+                            </div>
+                        </Link>
+                        <Link className='botoesHome pequeno'>
+                            <div>
+                                <h1>Alterar dados usuários</h1>
+                            </div>
+                        </Link>
                     </div>
-                </Link>
-                <Link>
+                )}
+                <Link className='botoesHome grande'>
                     <div>
                         <img src={foneAtendimento}/>
                         <h1> Iniciar um novo chamados</h1>
                     </div>
                 </Link>
-                <Link>
+                <Link className='botoesHome grande'>
                     <div>
                         <img src={papelComLapis}/>
                         <h1> Atender chamados</h1>
