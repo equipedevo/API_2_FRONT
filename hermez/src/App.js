caf9f9f80139c11f8bb42aa7ad1bcb5e5201398d
BrunoSerpa <serpabruno2005@gmail.com>
2023-10-24 06:30:52 -0300

    T01 - Adicionei o caminho para a lista dos funcionários;

--- hermez/src/App.js
+++ hermez/src/App.js
@@ -10,6 +10,7 @@ import CadastroEmpresa from './pages/CadastroEmpresa';
 import AbrirChamado from './pages/AbrirChamado';
 import AtenderChamado from './pages/AtenderChamados';
 import CadastroFuncionario from './pages/CadastroFuncionario';
+import TodosFuncionários from './pages/TodosFuncionários';
 
 function App() {
   return (
@@ -23,9 +24,11 @@ function App() {
           <Route path="realizar-chamado" element={< AbrirChamado />} />
           <Route path="atender-chamados" element={<AtenderChamado />} />
           <Route path="cadastrar-usuario" element={<CadastroFuncionario />} />
+          <Route path="lista-funcionarios" element={<TodosFuncionários />} />
         </Route>
         <Route path="/empresa" element={<BaseEmpresa />}>
           <Route index element={<CadastroFuncionario />} />
+          <Route path="lista-funcionarios" element={<TodosFuncionários />} />
         </Route>
         {/* path="*" serve para qualquer rota, então deve ficar por último e direcionar para a home ou uma página de erro 404 */}
         <Route path="*" element={<LoginFuncionario />} />
