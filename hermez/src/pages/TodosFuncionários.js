6e9c701a2d5e92ac3ef374248352b69ac583c032
BrunoSerpa <serpabruno2005@gmail.com>
2023-10-26 08:04:59 -0300

    T01 - Fiz a função filtrar Funcionários

--- hermez/src/pages/TodosFuncionários.js
+++ hermez/src/pages/TodosFuncionários.js
@@ -1,9 +1,21 @@
 import { useEffect, useState } from "react";
 import './css/TodosFuncionários.css';
 import lupa from '../img/lupa.png';
+import { Link } from "react-router-dom";
 export default function Funcionarios() {
     const [listaFuncionarios, setListaFuncionarios] = useState([]);
     const [erro, setErro] = useState('');
+    const [nomePesquisa, setNomePesquisa]= useState('');
+    const filtrarFuncionario = (nome, nomePesquisa) => {
+        nome = nome.toLowerCase();
+        nomePesquisa = nomePesquisa.toLowerCase();
+        if (nomePesquisa.length >= 3 && !nome.includes(nomePesquisa)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    } 
     useEffect(() => {
         fetch(process.env.REACT_APP_URL_TODOS_FUNCIONARIOS, {
             method:"POST",
@@ -18,20 +30,25 @@ export default function Funcionarios() {
         }).then(response => {
             if(response.status === 200) {
                 response.json().then((lista) => {
-                    setListaFuncionarios(lista.funcionarios);
+                    const listaFiltrada = lista.funcionarios.filter(funcionario =>
+                        filtrarFuncionario(funcionario.fun_nome, nomePesquisa)
+                    );
+                    setListaFuncionarios(listaFiltrada);
                 });
             }
             else{(response.json()).then(data => 
                 setErro(data.msg)
             )}
         })
-    }, []);
+    }, [nomePesquisa]);
     return (
         <>
             <div className="divTodosFuncionarios">
                 <div className="divPesquisarFuncionário">
                     <input
                         placeholder='Nome do Funcionário'
+                        value={nomePesquisa}
+                        onChange={(e) => setNomePesquisa(e.target.value)}
                     />
                     <img src={lupa} alt="lupa"/>
                 </div>
@@ -40,20 +57,56 @@ export default function Funcionarios() {
                     <div className="divTituloTabelaFuncionarios">
                         <p>Nome do Usuário</p>
                         <p>Serviço</p>
+                        <p>Editar dados</p>
                     </div>
                     <form className="divListaFuncionarios" >
-                        {listaFuncionarios.map(i =>
-                            <div>
-                                <p>{i.fun_nome}</p>
-                                <p>{
-                                    i.car_cod === 1 ? 'Funcionário':
-                                    i.car_cod === 2 ? 'Técnico':
-                                    i.car_cod === 3 ? 'Administrador':
-                                    'Unknown'}
-                                </p>
+                        {localStorage.getItem('fun_cod') !== null ?(listaFuncionarios.map
+                            (funcionario => funcionario.car_cod !== null &&
+                                <div key={funcionario.fun_cod}>
+                                    <p>{funcionario.fun_nome}</p>
+                                    <select className="selectTabelaFuncionarios">
+                                        <option value={funcionario.car_cod} style={{display: 'none'}}>{
+                                            funcionario.car_cod === 1 ? 'Funcionário':
+                                            funcionario.car_cod === 2 ? 'Técnico':
+                                            'Unknown'
+                                        }</option>
+                                        <option value="1">Funcionário</option>
+                                        <option value="2">Técnico</option>  
+                                    </select>
+                                    <div className="divButtonTabelaFuncionarios">
+                                        <Link to="">
+                                            Editar
+                                        </Link>
+                                    </div>
+                                </div>
+                            )
+                        ):(listaFuncionarios.map
+                            (funcionario =>
+                            <div key={funcionario.fun_cod}>
+                                <p>{funcionario.fun_nome}</p>
+                                <select className="selectTabelaFuncionarios">
+                                    <option value={funcionario.car_cod} style={{display: 'none'}}>{
+                                        funcionario.car_cod === 1 ? 'Funcionário':
+                                        funcionario.car_cod === 2 ? 'Técnico':
+                                        funcionario.car_cod === 3 ? 'Administrador':
+                                        'Unknown'
+                                    }</option>
+                                    <option value="1">Funcionário</option>
+                                    <option value="2">Técnico</option>  
+                                    <option value="3">Administrador</option>  
+                                </select>
+                                <div className="divButtonTabelaFuncionarios">
+                                    <Link to={`../editar-funcionario?idfuncionario=${funcionario.fun_cod}`}>
+                                        Editar
+                                    </Link>
+                                </div>
                             </div>
+                            )
                         )}
                     </form>
+                    {erro &&
+                        <p className="erro">{erro}</p>
+                    }
                 </div>
             </div>
         </>
