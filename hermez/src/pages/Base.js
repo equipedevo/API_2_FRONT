0d3a46396fcceadc1bec4f2216f88e52fd5c0fa1
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-05 12:15:53 -0300

    Retirei imports para './css/Base.css'; Arrumei colisão de estilização da classe vermelho

--- hermez/src/pages/Base.js
+++ hermez/src/pages/Base.js
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-import './css/Base.css';
-
 export default function Home(){
     if (localStorage.getItem('emp_cod') === null || localStorage.getItem('fun_cod') === null) {
         window.location.href = '/'
--- hermez/src/pages/BaseEmpresa.js
+++ hermez/src/pages/BaseEmpresa.js
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom'
 import Popup from '../components/Popup'
 import Header from '../components/HeaderEmpresa'
 import Footer from '../components/Footer'
-import './css/Base.css';
 
 export default function Home(){
     if (localStorage.getItem('emp_cod') === null || localStorage.getItem('fun_cod') !== null) {
