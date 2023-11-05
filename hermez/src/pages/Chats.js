9f419b04b5fff97f4c262f3fbe7cdddd75617e1e
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-05 18:20:22 -0300

    Arrumei conexao

--- hermez/src/pages/Chats.js
+++ hermez/src/pages/Chats.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import aberto from "../img/chats/iconeAberto.svg";
-import andamento from "../img/chats/iconeAndamento.svg";
-import reaberto from "../img/chats/iconeReaberto.svg";
-import cancelado from "../img/chats/iconeCancelado.svg";
-import concluido from "../img/chats/iconeConcluido.svg";
-import fechado from "../img/chats/iconeFechado.svg";
+//import andamento from "../img/chats/iconeAndamento.svg";
+//import reaberto from "../img/chats/iconeReaberto.svg";
+//import cancelado from "../img/chats/iconeCancelado.svg";
+//import concluido from "../img/chats/iconeConcluido.svg";
+//import fechado from "../img/chats/iconeFechado.svg";
 import anexo from "../img/chats/clipAnexo.svg";
 import enviar from "../img/chats/enviar.svg";
 import "./css/Chats.css";
@@ -20,10 +20,8 @@ export default function Chats() {
         {id: 2, nome: 'Média'},
         {id: 3, nome: 'Baixa'}
     ];
-
-
     useEffect(() => {
-        fetch(process.env.REACT_APP_URL_CHAMADO_GET_TODOS, {
+        fetch(`${process.env.REACT_APP_URL_CHAMADO_GET_TODOS}?dev=true`, {
             method: 'POST',
             body: JSON.stringify({
                 emp_cod: localStorage.getItem('emp_cod'),
@@ -36,56 +34,37 @@ export default function Chats() {
         })
             .then((response) => response.json())
             .then((data) => { setDatabase(data); })
-            .catch((error) => console.log(error));
-        
-        
+            .catch((error) => console.log(error));  
     })
-
-
     return (
         <>
             <div className="container">
-                <div className="lista">
-                {database.map(chamado => 
-                        
-                            <details onClick={() => setActiveChat(!activeChat)} key={chamado.cha_cod}>
-                                <summary className={activeChat ? 'chat active' : 'chat'} onClick={() => setMostraChat(!mostraChat)}>
-                                    <div className="icone">
-                                        <img src={aberto} alt='icone de aberto' />
-                                        <p className={`status ${chamado.sta_nome.toLowerCase()}`}>{ chamado.sta_nome }</p>
-                                    </div>
-                                    <div className="texto">
-                                        <p className="tituloChat aberto">{ chamado.cha_titulo }</p>
-                                        <p className="subtitulo">Você: Sua resposta como sempre foi aparentemente enorme</p>
-                                    </div>
-                                </summary>
-                                <div className="linhadotempo">
-
-                                </div>
-                            </details>
-
-                        
-                    
-                    )}
-
-                </div>
-
+                <div className="lista">{database.map(chamado =>
+                    <details onClick={() => setActiveChat(!activeChat)} key={chamado.cha_cod}>
+                        <summary className={activeChat ? 'chat active' : 'chat'} onClick={() => setMostraChat(!mostraChat)}>
+                            <div className="icone">
+                                <img src={aberto} alt='icone de aberto' />
+                                <p className={`status ${chamado.sta_nome.toLowerCase()}`}>{ chamado.sta_nome }</p>
+                            </div>
+                            <div className="texto">
+                                <p className="tituloChat aberto">{ chamado.cha_titulo }</p>
+                                <p className="subtitulo">Você: Sua resposta como sempre foi aparentemente enorme</p>
+                            </div>
+                        </summary>
+                        <div className="linhadotempo">
+                        </div>
+                    </details>  
+                )}</div>
                 <div className={mostraChat ? "conversa-fechada" : 'conversa-default'}>
                     <p>Inicie as conversas dos seus chamados</p>
                 </div>
-
                 <div className={mostraChat ? 'conversa-aberta' : 'conversa-fechada'} value={mostraChat}>
                     <p className="tituloConv aberto">Título do chamado</p>
-
                     <div className="mensagens">
-
                         <div className="balao pessoa_msg">
                             <p className="mensagem">messagem do cliente</p>
                         </div>
-
                     </div>
-
-
                     <div className="resposta">
                         <input className="campoResposta" placeholder="Digite sua mensagem..." type="text"></input>
                         <button type="file">
